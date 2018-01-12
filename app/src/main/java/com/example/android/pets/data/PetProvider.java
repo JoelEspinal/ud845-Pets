d2ed103a67cf579aa78ffd001153f4d6d3b6fc62
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2018-01-12 18:31:46 -0400

    Adding PetProvider (Content Provider) class, and declared the content Provider in manifest.xml

---
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -0,0 +1,78 @@
+package com.example.android.pets.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by User on 1/12/2018.
+ */
+
+public class PetProvider extends ContentProvider {
+
+    /** Tag for the log messages */
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+    /** Dabase helper object */
+    private PetDbHelper mDbHelper;
+
+
+    /**
+     * Initialize the provider and the database helper object.
+     */
+    @Override
+    public boolean onCreate() {
+        // TODO: Create and initialize a PetDbHelper object to gain access to the pets database.
+        // Make sure the variable is a global variable, so it can be referenced from other
+        // ContentProvider methods.
+        mDbHelper = new PetDbHelper(getContext());
+        return true;
+    }
+
+    /**
+     * Perform the query for the given URI. Use the given projection, selection, selection arguments, and sort order.
+     */
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+
+    /**
+     * Returns the MIME type of data for the content URI.
+     */
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    /**
+     * Insert new data into the provider with the given ContentValues.
+     */
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    /**
+     * Delete the data at the given selection and selection arguments.
+     */
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+
+    /**
+     * Updates the data at the given selection and selection arguments, with the new ContentValues.
+     */
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
